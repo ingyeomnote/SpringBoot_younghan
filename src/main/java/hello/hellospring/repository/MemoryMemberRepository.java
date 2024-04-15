9fd8f667b019093878075d09f621c3c5cc05e02f
KimInKyeom <freeartjin@naver.com>
2024-04-15 21:21:46 +0900

    스프링 입문 - 웹 개발 기초(백엔드 개발(1))

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -11,21 +11,21 @@ public class MemoryMemberRepository implements MemberRepository{
 
     @Override
     public Member save(Member member) {
-        member.setId(++sequence);
-        store.put(member.getId(), member);
+        member.setId(++sequence); // 가입을 하려는 회원에게 번호 부여
+        store.put(member.getId(), member); // 부여된 번호와 회원 정보 매핑
         return member;
     }
 
     @Override
     public Optional<Member> findById(Long id) {
-        return Optional.ofNullable(store.get(id));
+        return Optional.ofNullable(store.get(id)); // ofNullable -> null 일 수도?
     }
 
     @Override
     public Optional<Member> findByName(String name) {
-        return store.values().stream()
-                .filter(member -> member.getName().equals(name))
-                .findAny();
+        return store.values().stream() // store.values -> 값 꺼내고,
+                .filter(member -> member.getName().equals(name)) // 값 비교
+                .findAny(); // 일치하는 회원을 찾으변 반환 없으면 null
     }
 
     @Override
